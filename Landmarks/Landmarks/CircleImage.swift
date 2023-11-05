e032d6e1c1489bf60e6d31ec2119f83ced6741c2
shunsuke250 <soeyamashunsuke@gmail.com>
2023-11-05 14:52:54 +0900

    :sparkles: Section 4でCustom image viewを作成

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 副山俊輔 on 2023/11/05.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
