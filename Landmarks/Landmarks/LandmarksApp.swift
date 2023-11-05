50d2fc4b217325ce408d1596203bf077e7e7a1f0
shunsuke250 <soeyamashunsuke@gmail.com>
2023-11-05 14:40:53 +0900

    :sparkles: プロジェクトを作成

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 副山俊輔 on 2023/11/05.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
