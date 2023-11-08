6b363532e163af465c8fa3126267a8196e937f53
shunsuke250 <soeyamashunsuke@gmail.com>
2023-11-08 23:01:47 +0900

    :sparkles: Section 4でランドマークのリストを作成

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 副山俊輔 on 2023/11/08.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
