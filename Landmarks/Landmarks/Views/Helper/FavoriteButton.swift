1da8732ccf96ea93d468924d42ae09ec2b8731c5
shunsuke250 <soeyamashunsuke@gmail.com>
2023-11-11 22:02:11 +0900

    :+1: フォルダ構造を変更

---
+++ Landmarks/Landmarks/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,25 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by 副山俊輔 on 2023/11/11.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
