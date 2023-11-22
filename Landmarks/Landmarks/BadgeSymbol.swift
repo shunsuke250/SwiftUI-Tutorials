b3099d65058f55283383b5620df800af2a0e1e2a
shunsuke250 <soeyamashunsuke@gmail.com>
2023-11-22 23:16:12 +0900

    :+1: スペースを削除

--- Landmarks/Landmarks/BadgeSymbol.swift
+++ Landmarks/Landmarks/BadgeSymbol.swift
@@ -29,7 +29,7 @@ struct BadgeSymbol: View {
                     CGPoint(x: middle + topWidth, y: topHeight - spacing),
                     CGPoint(x: middle, y: spacing)
                 ])
-                
+
                 path.move(to: CGPoint(x: middle, y: topHeight / 2 + spacing * 3))
                 path.addLines([
                     CGPoint(x: middle - topWidth, y: topHeight + spacing),
